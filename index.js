5ee4a063ce11e0557361acee21e5fc4f9c9dbba0
linder <achie_31@yahoo.com>
2024-09-22 21:38:57 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const messages = []; 
+  
+    for (let i = 0; i < names.length; i++) {
+      
+      const message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      
+      
+      messages.push(message);
+    }
+  
+    
+    return messages;
+  }
+  
+  
+  const names = ["Guadalupe", "Ollie", "Aki"];
+  const event = "surprise";
+  
+  const thankYouMessages = writeCards(names, event);
+  
+  console.log(thankYouMessages);
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number); // Log the current number
+      number--; // Decrement the number by 1
+    }
+  }
+  
